ed849a8891aca939e1feba9b42b4923f3f727644
fujioka <fujioka8700@gmail.com>
2023-11-25 07:14:06 +0900

    [add] Vue にて TypeScript を使用する。

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,6 +2,7 @@ import "./bootstrap";
 import "../css/app.css";
 
 import { createApp, h } from "vue";
+import type { DefineComponent } from "vue";
 import { createInertiaApp, router } from "@inertiajs/vue3";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
@@ -19,10 +20,10 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob("./Pages/**/*.vue")
+            import.meta.glob<DefineComponent>("./Pages/**/*.vue")
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
@@ -31,3 +32,5 @@ createInertiaApp({
         color: "#4B5563",
     },
 });
+
+import "./sample.ts";
---
+++ resources/js/sample.ts
@@ -0,0 +1,8 @@
+const oneToFive = [1, 2, 3, 4, 5];
+const [one, ...rest] = oneToFive;
+
+console.log(one);
+// 1
+
+console.log(rest);
+// [ 2, 3, 4, 5 ]
