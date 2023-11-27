77c11a4bfae6c9b4232fd76630387efc6c0f7f3f
fujioka <fujioka8700@gmail.com>
2023-11-27 09:20:22 +0900

    [update] アサーション関数

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -10,11 +10,18 @@ class Duck {
     }
 }
 
+function assertIsDefined<T>(val: unknown): asserts val is NonNullable<T> {
+    if (val === undefined || val === null) {
+        throw new Error(`Expected 'val' to be defined, but received ${val}`);
+    }
+}
+
 function isDuck(animal: Animal | Duck): animal is Duck {
     return animal instanceof Duck;
 }
 
-function animalOrDuck(animal: Animal | Duck): void {
+function animalOrDuck(animal: Animal | Duck | null): void {
+    assertIsDefined(animal);
     if (isDuck(animal)) {
         console.log(animal.quacks());
     } else {
@@ -27,3 +34,5 @@ animalOrDuck(duck);
 
 const animal = new Animal();
 animalOrDuck(animal);
+
+animalOrDuck(null);
