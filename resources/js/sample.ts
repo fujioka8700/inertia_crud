852dbe1b5566cdcd5670217592431e1fb1a9f8dd
fujioka <fujioka8700@gmail.com>
2023-12-03 08:42:38 +0900

    [update] クラスの readonly 修飾子

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,26 +1,16 @@
-// example.ts
-class ConstructorInAccessModifier {
-    constructor(
-        arg0: number,
-        public arg1: number,
-        protected arg2: number,
-        private arg3: number
-    ) {
-        console.log({ arg0, arg1, arg2, arg3 });
+class Octopus {
+    readonly name: string;
+    readonly legs = 8; // フィールド初期化子での代入はOK
+
+    constructor() {
+        this.name = "たこちゃん"; // コンストラクターでの代入はOK
+    }
+
+    setName(newName: string) {
+        this.name = newName;
     }
 }
 
-class ConstructorOutAccessModifier {
-    public arg1: number;
-    protected arg2: number;
-    private arg3: number;
-    constructor(arg0: number, arg1: number, arg2: number, arg3: number) {
-        this.arg1 = arg1;
-        this.arg2 = arg2;
-        this.arg3 = arg3;
-        console.log({ arg0, arg1, arg2, arg3 });
-    }
-}
-
-const obj = new ConstructorInAccessModifier(1, 2, 3, 4);
-const obj2 = new ConstructorOutAccessModifier(10, 20, 30, 40);
+const oct: Octopus = new Octopus();
+oct.setName("新しいたこちゃん");
+console.log(oct.name);
