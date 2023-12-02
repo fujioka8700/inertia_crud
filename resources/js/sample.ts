491ce7163d6e34467a522d34248b8948dbe5042f
fujioka <fujioka8700@gmail.com>
2023-12-02 15:02:42 +0900

    [update] アクセス修飾子

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,7 +1,20 @@
 class Point {
-    x: number = 0;
+    protected x: number = 0;
     y: number = 0;
+
+    getX() {
+        return this.x;
+    }
 }
-const point = new Point();
-console.log(point.x, point.y);
+
+class PointX extends Point {
+    getPointX() {
+        return this.x;
+    }
+}
+
+const point = new PointX();
+// console.log(point.x, point.y);
 // 0 0
+
+console.log(point.getX());
