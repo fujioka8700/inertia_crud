9df62180698a4a8894c69e1143762d42d5be584a
fujioka <fujioka8700@gmail.com>
2023-12-02 14:36:20 +0900

    [update] class の基礎

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,20 +1,5 @@
-// 1秒後に値を返す
-function request(): Promise<string> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("hello");
-        }, 1000);
-    });
-}
+class Person {}
 
-// この書き方はできない
-// const result = await request();
-// console.log(result);
+const person: Person = new Person();
 
-async function main() {
-    const result = await request();
-    console.log(result);
-    // @log: "hello"
-}
-
-main();
+console.log(person);
