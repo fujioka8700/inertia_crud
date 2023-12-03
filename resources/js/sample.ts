651e2657b0abd2f5b9edcc98240ea4ae7035669c
fujioka <fujioka8700@gmail.com>
2023-12-03 18:20:41 +0900

    [update] メソッド戻り値のthis型と、メソッドチェーン

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,8 +1,33 @@
-// TypeScript
-class SomeClass {
-    private static doSomething() {
-        console.log("doSomething");
+class Operator {
+    public value: number;
+
+    public constructor(value: number) {
+        this.value = value;
+    }
+
+    public sum(value: number): this {
+        this.value += value;
+        return this;
+    }
+
+    public subtract(value: number): Operator {
+        this.value -= value;
+        return this;
+    }
+
+    public multiply(value: number): void {
+        this.value *= value;
+    }
+
+    public divide(value: number): void {
+        this.value /= value;
     }
 }
 
-SomeClass.doSomething();
+const op: Operator = new Operator(0);
+op.sum(5).subtract(2).multiply(5); // 15
+console.log(op.value);
+
+// op.subtract(3); // 2
+// op.multiply(6); // 12
+// op.divide(3); // 4
