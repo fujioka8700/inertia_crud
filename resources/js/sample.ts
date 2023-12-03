df7d8540bf20e5ede8f331d02bfd59c70c96ac9d
fujioka <fujioka8700@gmail.com>
2023-12-03 19:21:54 +0900

    [update] 構造的部分型ではなく交渉型になる。

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,56 +1,19 @@
-//親クラス
+class UserId {
+    private readonly id: string;
 
-abstract class Department {
-    static fiscalYear = 2021;
-    protected employees: string[] = [];
-
-    static createEmployee(name: string) {
-        return { name: name };
-    }
-
-    constructor(protected readonly id: string, public name: string) {
-        console.log(Department.fiscalYear);
-    }
-
-    abstract describe(this: Department): void;
-}
-
-//サブクラス1
-
-class ITDepartment extends Department {
-    admins: string[];
-    constructor(id: string, admins: string[]) {
-        super(id, "IT");
-        this.admins = admins;
-    }
-
-    //親クラスのabstractメソッドをサブクラスで実装する必要がある
-    describe() {
-        console.log("IT部署 - ID: " + this.id);
+    constructor(id: string) {
+        this.id = id;
     }
 }
 
-//サブクラス2
+class GroupId {
+    private readonly id: string;
 
-class AccountingDepartment extends Department {
-    private lastReport: string | undefined;
-
-    constructor(id: string, private reports: string[]) {
-        super(id, "Accounting");
-        this.lastReport = reports[0];
-    }
-
-    //親クラスのabstractメソッドをサブクラスで実装する必要がある
-    describe() {
-        console.log("会計部署 - ID: " + this.id);
+    constructor(id: string) {
+        this.id = id;
     }
 }
 
-const kaikei: AccountingDepartment = new AccountingDepartment("100", [
-    "tanaka",
-    "sato",
-]);
-kaikei.describe();
-
-const it: ITDepartment = new ITDepartment("200", ["goto", "kimura"]);
-it.describe();
+const userId: UserId = new GroupId("...");
+// Type 'GroupId' is not assignable to type 'UserId'.
+//   Types have separate declarations of a private property 'id'.
