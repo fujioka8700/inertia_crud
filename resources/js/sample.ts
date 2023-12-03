77a103dfaca756b8d58bcb8259a1b57a107050d3
fujioka <fujioka8700@gmail.com>
2023-12-03 18:24:53 +0900

    [update] クラスの継承。スーパークラスとサブクラス。

--- resources/js/sample.ts
+++ resources/js/sample.ts
@@ -1,33 +1,12 @@
-class Operator {
-    public value: number;
-
-    public constructor(value: number) {
-        this.value = value;
+class Parent {
+    constructor() {
+        console.log("This is Parent.");
     }
-
-    public sum(value: number): this {
-        this.value += value;
-        return this;
-    }
-
-    public subtract(value: number): Operator {
-        this.value -= value;
-        return this;
-    }
-
-    public multiply(value: number): void {
-        this.value *= value;
-    }
-
-    public divide(value: number): void {
-        this.value /= value;
+}
+class Child extends Parent {
+    constructor() {
+        super();
     }
 }
 
-const op: Operator = new Operator(0);
-op.sum(5).subtract(2).multiply(5); // 15
-console.log(op.value);
-
-// op.subtract(3); // 2
-// op.multiply(6); // 12
-// op.divide(3); // 4
+new Child();
